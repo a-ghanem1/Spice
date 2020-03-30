f1d861f685d041d5cfbfadf1835f173f4443c765
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-03-30 09:23:06 +0200

    Finishing Delete Category section

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -65,6 +65,7 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
         }
 
+        // Post - Edit
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Category category)
@@ -78,6 +79,42 @@ namespace Spice.Areas.Admin.Controllers
             }
 
             return View(category);
+        }
+
+        // GET - Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // Post - Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return View();
+            }
+
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
