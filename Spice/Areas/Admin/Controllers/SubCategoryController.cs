80728c0d1014d26b6fdc0cdc2ccb47988526e5ab
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-03-30 17:03:59 +0200

    Finishing Delete SubCategory section

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -143,5 +143,22 @@ namespace Spice.Areas.Admin.Controllers
             };
             return View(modelVM);
         }
+
+        public async Task<IActionResult> Details(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _db.SubCategory.Include(s => s.Category).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
     }
 }
