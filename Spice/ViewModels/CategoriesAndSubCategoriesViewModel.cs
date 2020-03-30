0e9da2cf4fc2dade37f31ee3f7aa9047c6437c13
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-03-30 12:51:40 +0200

    Finshing Create SubCategory section

---
+++ Spice/ViewModels/CategoriesAndSubCategoriesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.ViewModels
+{
+    public class CategoriesAndSubCategoriesViewModel
+    {
+        public IEnumerable<Category> CategoriesList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoriesList { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
