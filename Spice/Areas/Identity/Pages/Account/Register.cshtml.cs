94fd2f4dffd06af7f9b0478bdd30c6a81ac45e1e
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-04-01 15:32:52 +0200

    Regestering new Emplyees section

--- Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,8 @@ namespace Spice.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            string role = Request.Form["rdUserRole"].ToString();
+
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
@@ -113,8 +115,32 @@ namespace Spice.Areas.Identity.Pages.Account
                         await _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser));
                     }
 
-                    await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                    if (role == SD.KitchenUser)
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.KitchenUser);
+                    }
+                    else
+                    {
+                        if (role == SD.FrontDeskUser)
+                        {
+                            await _userManager.AddToRoleAsync(user, SD.FrontDeskUser);
+                        }
+                        else
+                        {
+                            if (role == SD.ManagerUser)
+                            {
+                                await _userManager.AddToRoleAsync(user, SD.ManagerUser);
+                            }
+                            else
+                            {
+                                await _userManager.AddToRoleAsync(user, SD.CustomerEndUser);
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                return LocalRedirect(returnUrl);
+                            }
+                        }
+                    }
 
+                    return RedirectToAction("Index", "User", new { area = "Admin" });
                     _logger.LogInformation("User created a new account with password.");
 
                     //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
@@ -133,9 +159,6 @@ namespace Spice.Areas.Identity.Pages.Account
                     //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
                     //}
                     //else
-                    //{
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
                     //}
                 }
                 foreach (var error in result.Errors)
