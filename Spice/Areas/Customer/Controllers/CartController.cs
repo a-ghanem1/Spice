d38d81a1e1c68b01f80467e6450299b8ad526dc5
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-04-02 13:55:58 +0200

    Finish the Index ShoppingCart section

---
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Utility;
+using Spice.ViewModels;
+
+namespace Spice.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCart DetailCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DetailCart = new OrderDetailsCart() 
+            { 
+                OrderHeader= new Models.OrderHeader()
+            };
+
+            DetailCart.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+            if (cart != null)
+            {
+                DetailCart.CartList = cart.ToList();
+            }
+
+            foreach (var list in DetailCart.CartList)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                DetailCart.OrderHeader.OrderTotal = DetailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+                list.MenuItem.Description = SD.ConvertToRawHtml(list.MenuItem.Description);
+                if (list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            DetailCart.OrderHeader.OrderTotalOriginal = DetailCart.OrderHeader.OrderTotal;
+
+            return View(DetailCart);
+        }
+    }
+}
---
+++ Spice/ViewModels/OrderDetailsCart.cs
@@ -0,0 +1,14 @@
+﻿using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.ViewModels
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> CartList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
